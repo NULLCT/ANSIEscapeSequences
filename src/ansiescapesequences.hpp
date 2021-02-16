322eb6fd41483f19bc5d107f560facfe806b28dc
NULLCT <cockatiel.u10@gmail.com>
2021-02-16 17:38:45 +0900

    attach cursor moving

--- src/ansiescapesequences.hpp
+++ src/ansiescapesequences.hpp
@@ -1,5 +1,33 @@
 #include <string>
+// https://qiita.com/PruneMazui/items/8a023347772620025ad6
+using namespace std;
 
-namespace ess{
-  string cursorUp(const int _n)
+namespace es{
+  //cursor moving
+  string moveUp(int n){
+    return "\e[" + to_string(n) + "A";
+  }
+  string moveDown(int n){
+    return "\e[" + to_string(n) + "B";
+  }
+  string moveRight(int n){
+    return "\e[" + to_string(n) + "C";
+  }
+  string moveLeft(int n){
+    return "\e[" + to_string(n) + "D";
+  }
+
+  string moveDownLine(int n){
+    return "\e[" + to_string(n) + "E";
+  }
+  string moveUpLine(int n){
+    return "\e[" + to_string(n) + "F";
+  }
+
+  string moveCol(int n){
+    return "\e[" + to_string(n) + "G";
+  }
+  string move(int line,int col){
+    return "\e[" + to_string(line) + ";" + to_string(col) + "H";
+  }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <ostream>
+#include "ansiescapesequences.hpp"
 using namespace std;
 int main(){
-
+  for(int i=0;i<100;i++){
+    cout<<es::move(i,i);
+    cout<<i;
+  }
+  cout<<flush;
 }
